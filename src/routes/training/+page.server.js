cab972db6d5442c60d035dcadf4c2fde1c172119
NabilTouri <tourinabil3@gmail.com>
2024-03-07 23:55:40 +0100

    update

--- src/routes/training/+page.server.js
+++ src/routes/training/+page.server.js
@@ -3,7 +3,7 @@ import { redirect } from "@sveltejs/kit"
 
 export const load = async ({ cookies, url }) => {
     if (!cookies.get('username')) {
-        throw redirect(307, `/register?redirectTo=${url.pathname}`)
+        throw redirect(307, `/login?redirectTo=${url.pathname}`)
     }
     // console.log("Home page universal load function called")
     const title = "Training Page";
